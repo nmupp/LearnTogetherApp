4677732694a8d1eee107bcb50c6006e6cc373cf6
Narasimhulu Muppalla <nmupphere@gmail.com>
2024-05-30 20:03:07 -0500

    compose quadrant

--- app/src/main/java/com/example/learntogetherapp/MainActivity.kt
+++ app/src/main/java/com/example/learntogetherapp/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -30,7 +34,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LearnTogetherAppTheme {
-                ComposeTutorial()
+                TaskView()
             }
         }
     }
@@ -71,29 +75,71 @@ fun ComposeTutorial() {
 @Composable
 fun TaskView() {
     val image = painterResource(R.drawable.ic_task_completed)
-    Image(painter = image, contentDescription = "Task view")
-    Text(
-        text = "All tasks completed",
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
-        textAlign = TextAlign.Center
-    )
-    Text(
-        text = "Nice work!"
-    )
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = image, contentDescription = "Task view")
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Nice work!"
+        )
+    }
+
 }
+
+@Composable
+private fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+@Composable
+private fun ComposeQuadrants() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(title = "Text composable", description = "Displays text and follows the recommended Material Design guidelines.", backgroundColor = Color(0xFFEADDFF), modifier = Modifier.weight(1f))
+            ComposableInfoCard(title = "Image composable", description = "Creates a composable that lays out and draws a given Painter class object.", backgroundColor = Color(0xFFD0BCFF), modifier = Modifier.weight(1f))
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(title = "Row composable", description = "A layout composable that places its children in a horizontal sequence.", backgroundColor = Color(0xFFB69DF8), modifier = Modifier.weight(1f))
+            ComposableInfoCard(title = "Column composable", description = "A layout composable that places its children in a vertical sequence.", backgroundColor = Color(0xFFF6EDFF), modifier = Modifier.weight(1f))
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun TaskCompletePreview() {
     LearnTogetherAppTheme {
+        ComposeQuadrants()
 //        ComposeTutorial()
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            TaskView()
-        }
+//        TaskView()
     }
 }
